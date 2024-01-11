f98b7a683b0b222f1b6d2dccc41e772bfc6b0f82
Anthony Durandal <a.durandal@outlook.com>
2024-01-11 23:41:45 +0300

    style: add separator to header-little-nav

--- client/src/features/common-components/header/header-little-nav.tsx
+++ client/src/features/common-components/header/header-little-nav.tsx
@@ -6,28 +6,31 @@ export const HeaderLittleNav = () => {
     <div className="hidden min-w-full h-9 md:flex justify-between px-9 bg-secondary">
       <div className="h-full flex">
         <IconContainer>
-            <JordanIcon className="h-6 w-6" />
+          <JordanIcon className="h-6 w-6" />
         </IconContainer>
         <IconContainer>
           <ConverseIcon className="h-6 w-6" />
         </IconContainer>
       </div>
-      <nav className="flex h-full text-xs gap-2">
+      <nav className="flex h-full text-xs gap-1">
         <HeaderLittleNavLink title="Find a Store" />
+        <NavSeparator />
         <HeaderLittleNavLink title="Help" />
+        <NavSeparator />
         <HeaderLittleNavLink title="Join Us" />
+        <NavSeparator />
         <HeaderLittleNavLink title="Sign In" />
       </nav>
     </div>
   );
 };
 
-const HeaderLittleNavLink = (props: {title: string}) => {
-    return (
-      <div className="px-2 flex items-center">
-        <p className="font-bold text-primary">{props.title}</p>
-      </div>
-    );
+const HeaderLittleNavLink = (props: { title: string }) => {
+  return (
+    <div className="px-2 py-1 flex items-center">
+      <p className="font-medium text-primary">{props.title}</p>
+    </div>
+  );
 };
 
 const IconContainer = (props: { children: ReactNode }) => {
@@ -36,4 +39,8 @@ const IconContainer = (props: { children: ReactNode }) => {
       <a href="#">{props.children}</a>
     </div>
   );
+};
+
+const NavSeparator = () => {
+  return <span className="flex items-center">|</span>;
 };
