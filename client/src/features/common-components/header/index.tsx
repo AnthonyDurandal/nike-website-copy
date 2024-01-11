b9d329cc4e8f7a7348ffc604b604ee2d53e7c743
Anthony Durandal <a.durandal@outlook.com>
2024-01-11 23:42:07 +0300

    update imports

--- client/src/features/common-components/header/index.tsx
+++ client/src/features/common-components/header/index.tsx
@@ -1,5 +1,6 @@
 export * from "./header";
 export * from "./header-little-nav";
 export * from "./header-nav";
+export * from "./header-nav/";
 export * from "./nav-dropdown";
 export * from "./search-bar";
